e788b09517f7a843713d31b08c9df862394c323b
obuidulpias <obuidul.pias@gmail.com>
2022-10-09 00:34:19 +0600

    curd operation with API

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('./db/connect')
+
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
 
@@ -17,4 +18,15 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3001 
 
-app.listen(port, console.log(`Server is listening on port ${port}....`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}....`))
+    } catch (error){
+          console.log(error)  
+    }
+}
+
+start()
+
+
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb://localhost:27017/TASK-MANAER'
 
-mongoose
-.connect(connectionString).then(()=>console.log('CONNECTED TO THE DB......'))
-.catch((err)=>console.log(err))
+const connectDB = (url) => {
+    return mongoose
+    .connect(url)
+}
+
+module.exports = connectDB
